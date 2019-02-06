4f820b3a61b0662596dfa10b2525d4e2b593800a
DanielKRing1 <DanielKRing1@gmail.com>
2019-02-05 17:16:18 -0800

    Modularized express routes

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const bodyParser = require('body-parser');
 
 // MODELS
 const User = require('./models/User');
-const Post = require('./models/Post');
+const { Post } = require('./models/Post');
 const Transaction = require('./models/Transaction');
 
 // DATABASE
-const MONGO_PATH = 'mongodb://localhost:27017/inFridge';
+const MONGO_PATH = process.env.MONGODB_URI || 'mongodb://localhost:27017/inFridge';
 const db = require('./database');
 db(MONGO_PATH);
 
@@ -22,42 +22,50 @@ const app = express();
 app.use(helmet());
 app.use(bodyParser.json());
 
+// ROUTES (from'routes/index')
+const routes = require('./routes');
+const router = express.Router();
+routes(router);
+app.use(router);
+
 app.get('/', (req, res)=> {
     res.send({
         Hello: 'World'
     });
 });
 
-app.get('/get', (req, res) => {
+app.get('/:userId/post/:postId', (req, res) => {
+    const id = req.params.id;
+    console.log(id);
 
+    respondWithDataById(res, User, id);
+});
+app.get('/:userId/all-posts', (req, res) => {
+    const id = req.params.id;
+    console.log(id);
+
+    getDocByIdAndRespond(res, User, id);
 });
 
-app.post('/create-user', async(req, res) => {
-    console.log(req.body);
+// app.post('/:userId/transaction', async(req, res) => {
+//     console.log(req.body);
 
-    const { name, password } = req.body;
+//     const userId = req.params.userId;
+//     const { body, location, tradeType } = req.body;
 
-    const user = new User({
-        name,
-        password
-    });
-    try{
-        const newUser = await user.save();
+//     const transaction = new Transaction({
+//         author: userId,
+//         body,
+//         location,
+//         tradeType
+//     });
 
-        res.status(201).send({
-            response: 'New User created!',
-            User: newUser
-        });
-    }catch(err) {
-        console.log(err.name+'\n');
-        console.log(err.code+'\n');
-        console.log(err.message);
+//     await saveDocAndRespond(res, transaction);
+// });
 
-        res.status(409).send({
-            error: err.name,
-            message: err.message
-        });
-    }
-});
+// SHORTCUTS ---------
+const str2Id = (str) => {
+    return mongoose.Types.ObjectId(str);
+}
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
---
+++ utility/responses.js
@@ -0,0 +1,65 @@
+const transaction = require('../models/Transaction');
+module.exports = {
+    // For POST requests
+    saveDocAndRespond: async(res, doc) => {
+        try{
+            const newDoc = await doc.save();
+
+            // console.log(newDoc._id)
+
+            // Transaction.likePost(newDoc._id);
+            // Transaction.complete(newDoc._id, "5c5770509109e157040dfffb");
+
+            res.status(201).send({
+                response: 'Document successfully Saved!',
+                data: newDoc
+            });
+        }catch(err) {
+            console.log(err.name+'\n');
+            console.log(err.code+'\n');
+            console.log(err.message);
+
+            res.status(409).send({
+                error: err.name,
+                message: err.message
+            });
+        }
+    },
+
+    // For GET requests
+    getDocByIdAndRespond: async(res, model, id) => {
+        try{
+            const doc = await model.findById(id);
+            res.status(200).send({
+                data: doc
+            });
+        }catch(err) {
+            console.log(err);
+            res.status(404).send({
+                response: err.name,
+                message: err.message
+            });
+        }
+    },
+
+    populateAndRespond: async(res, model, id, property) => {
+        try {
+            await model.findOne({ _id: id })
+            .populate(property)
+            .exec(function(err, doc) {
+                if(err) throw new Error();
+
+                res.status(200).send({
+                    data: doc[property]
+                });
+            });
+
+        }catch(err) {
+            console.log(err);
+            res.status(404).send({
+                response: err.name,
+                message: err.message
+            });
+        }
+    }
+}
