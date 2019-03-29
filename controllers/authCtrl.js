718815ef52caea5dc50c3e0147317c938bb28624
Victoria Hong <victoria.hong@student.csulb.edu>
2019-03-28 18:05:20 -0700

    printing URL by oath

--- controllers/authCtrl.js
+++ controllers/authCtrl.js
@@ -3,48 +3,69 @@ const User = require('../models/User');
 
 var passport = require('passport');
 
+const credentials = {
+  client: {
+    id: process.env.APP_ID,
+    secret: process.env.APP_PASSWORD,
+  },
+  auth: {
+    tokenHost: 'https://login.microsoftonline.com',
+    authorizePath: 'common/oauth2/v2.0/authorize',
+    tokenPath: 'common/oauth2/v2.0/token'
+  }
+};
+
+const oauth2 = require('simple-oauth2').create(credentials);
+
 
 // const { authenticateUser } = require('../MicrosoftGraph/microsoftGraph');
 
+
 module.exports = {
-        authenticateUser: async (req, res, next) => {
-          console.log("Before redirecting");
-          // res.send("POST request to go to passport.authenticate");
-          console.log("should go to passport.authenticate now");
-          await passport.authenticate('azuread-openidconnect',
-            {
-              response: res,
-              prompt: 'login',
-              failureRedirect: '/',
-              failureFlash: true
-            }
-          )(req,res,next);
-          next();
+        authenticateUser: async (req, res) => {
+          // console.log("Before redirecting");
+          // // res.send("POST request to go to passport.authenticate");
+          // console.log("should go to passport.authenticate now");
+          // await passport.authenticate('azuread-openidconnect',
+          //   {
+          //     response: res,
+          //     prompt: 'login',
+          //     failureRedirect: '/',
+          //     failureFlash: true
+          //   }
+          // )(req,res,next);
+          // next();
+          const returnVal = oauth2.authorizationCode.authorizeURL({
+            redirect_uri: process.env.REDIRECT_URI,
+            scope: process.env.APP_SCOPES
+          });
+          console.log(`Generated auth url: ${returnVal}`);
+          return returnVal;
         },
-        authenticateUserRedirect: (req, res) => {
-          console.log("passport.authenticate callback");
-          res.redirect('/');
-        },
+        // authenticateUserRedirect: (req, res) => {
+        //   console.log("passport.authenticate callback");
+        //   res.redirect('/');
+        // },
 
-        callback: async (req, res, next) => {
-          console.log("called callback");
-          await passport.authenticate('azuread-openidconnect',
-            {
-              response: res,
-              failureRedirect: '/',
-              failureFlash: true
-            }
-          )(req,res,next);
-          next();
-        },
+        // callback: async (req, res, next) => {
+        //   console.log("called callback");
+        //   await passport.authenticate('azuread-openidconnect',
+        //     {
+        //       response: res,
+        //       failureRedirect: '/',
+        //       failureFlash: true
+        //     }
+        //   )(req,res,next);
+        //   next();
+        // },
 
-        callbackRedirect: (req, res) => {
-          console.log("callback redirect");
-          // TEMPORARY!
-          // Flash the access token for testing purposes
-          req.flash('error_msg', {message: 'Access token', debug: req.user.accessToken});
-          res.redirect('/');
-        },
+        // callbackRedirect: (req, res) => {
+        //   console.log("callback redirect");
+        //   // TEMPORARY!
+        //   // Flash the access token for testing purposes
+        //   req.flash('error_msg', {message: 'Access token', debug: req.user.accessToken});
+        //   res.redirect('/');
+        // },
 
         signIn: (req, res) => {
               //{ id, password } = req.body;
